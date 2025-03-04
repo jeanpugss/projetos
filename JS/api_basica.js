2ad26c1ea95160602caf5aa7404401be04f01707
Jean Robert Pugss <jean.pugss@live.com>
2025-03-04 17:27:11 -0300

    updates

--- JS/api_basica.js
+++ JS/api_basica.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Olá, mundo!');
+    res.send('Olá, mundo! 2');
 }
 );
 
-app.listen(3001, () => {
-    console.log('Servidor rodando na porta 3001');
+app.listen(4001, () => {
+    console.log('Servidor rodando na porta 4001');
 });
--- JS/server.mjs
+++ JS/server.mjs
@@ -3,12 +3,12 @@ import { createServer } from 'node:http';
 
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Muito Massa World!\n');
+  res.end('Muito Massa coisa de api basica hein World!\n');
 });
 
 // starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+server.listen(3001, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3001');
 });
 
 // run with `node server.mjs`
